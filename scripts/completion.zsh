03c4b0a15882f08ccfaf65600dd6c3f382d8a0db
Caspian Baska <email@caspian.computer>
2022-04-27 12:26:03 +1000

    feat: further work on zsh completion

--- scripts/completion.zsh
+++ scripts/completion.zsh
@@ -1,64 +1,70 @@
 #compdef placeos
 
-local -a _1st_arguments
-_1st_arguments=(
-  'start:Start the environment.'
-  'stop:Stops the environment.'
-  'task:Runs a task in the environment.'
-  'changelog:Displays platform changelog (in markdown).'
-  'update:Update the platform version.'
-  'upgrade:Upgrade the Partner Environment.'
-  'uninstall:Uninstalls the Partner Environment.'
-  'help:Display this message.'
+_placeos() {
+
+_placeos_commands() {
+    local -a commmands; commands=(
+    'start:start the environment.' \
+    'stop:stops the environment.' \
+    'task:runs a task in the environment.' \
+    'changelog:displays platform changelog (in markdown).' \
+    'update:update the platform version.' \
+    'upgrade:upgrade the Partner Environment.' \
+    'uninstall:uninstalls the Partner Environment.' \
+    'help:display this message.' \
+    )
+    _describe -t commands 'PlaceOS commmand' commands
+}
+
+
+local -a _start_arguments; _start_arguments=(
+  '--email:email to setup an admin account for.' \
+  '--password:password for created admin account.' \
+  '--application:application to configure.' \
+  '--domain:domain to configure.' \
+  '--hard-reset:reset the environment to a default state.' \
+  $verbose_args \
+  $help_args \
 )
 
-local -a _start_arguments
-_start_arguments=(
-  '--email:Email to setup an admin account for.'
-  '--password:Password for created admin account.'
-  '--application:Application to configure.'
-  '--domain:Domain to configure.'
-  '--hard-reset:Reset the environment to a default state.'
-  '-v:Write logs to STDOUT in addition to the log file.'
-  'h:Display this message'
+local -a help_args; help_args=(
+  '(-h --help)[show help]'
 )
 
-local -a _stop_arguments
-_stop_arguments=(
-  '-v:Write logs to STDOUT in addition to the log file.'
-  '-h:Display this message.'
+local -a verbose_args; verbose_args=(
+  '(-v --verbose)'{-v,--verbose}'[write logs to STDOUT in addition to the log file]'
 )
 
-local -a _task_arguments
-_task_arguments=(
-  '-t:--tasks:Display list of available tasks.'
-  '-h:Display this message.'
+local -a _stop_arguments; _stop_arguments=(
+  $verbose_args \
+  $help_args \
 )
 
-local -a _changelog_arguments
-_changelog_arguments=(
-  '--full:Include all prior CHANGELOG entries.'
-  '-h:Display this message'
+local -a _task_arguments; _task_arguments=(
+  '-t:--tasks:display list of available tasks.' \
+  $help_args \
 )
 
-local -a _update_arguments
-_update_arguments=(
-  '--list:List the available versions.'
-  '-v:--verbose:Write logs to STDOUT in addition to the log file.'
-  '-h:Display this message.'
+local -a _changelog_arguments; _changelog_arguments=(
+  '--full:include all prior CHANGELOG entries.' \
+  $help_args \
 )
 
-local -a _upgrade_arguments
-_upgrade_arguments=(
-  '--list:Lists versions of the Partner Environment.'
-  '-q:Do not write command logs to STDOUT.'
-  '-h:Display this message.'
+local -a _update_arguments; _update_arguments=(
+  '--list:list the available versions.' \
+  $verbose_args \
+  $help_args \
 )
 
-local -a _uninstall_arguments
-_uninstall_arguments=(
-  '--force:Skip confirmation of uninstall.'
-  '-h:Display this message.'
+local -a _upgrade_arguments; _upgrade_arguments=(
+  '--list:lists versions of the Partner Environment.' \
+  '-q:do not write command logs to STDOUT.' \
+  $help_args \
+)
+
+local -a _uninstall_arguments; _uninstall_arguments=(
+  '--force:skip confirmation of uninstall.' \
+  $help_args \
 )
 
 _arguments -C \
@@ -67,7 +73,7 @@ _arguments -C \
 
 case $state in
   (subcommand)
-    _describe -t commands 'PlaceOS subcommand' _1st_arguments && return 0
+    _placeos_commands && return 0
     ;;
   (task)
     case $line[1] in
@@ -97,4 +103,8 @@ case $state in
         ;;
     esac
     ;;
-esac-
+esac
+
+}
+
+_placeos
